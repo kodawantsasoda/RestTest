81bbaa2d1144a21d673e86b3d242148d456805af
kodawantsasoda <dkotarvera@gmail.com>
2023-10-31 05:48:17 -0700

    done
    finished

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,18 +1,33 @@
 const express = require('express')
 const router = express.Router()
 const Subscriber = require('../models/subscriber')
+const subscriber = require('../models/subscriber')
 
 //getting all
-router.get('/', (req, res) =>{
-    res.send('Hello World')
+router.get('/', async(req, res) =>{
+    try{
+        const subscribers = await Subscriber.find()
+        res.json(subscribers)
+    }catch(err){
+      res.status(500).json({message: err.message})  
+    }
 })
 //getting one
 router.get('/:id', (req, res) => {
     res.send(req.params.id)
 })
 //creating one
-router.post('/', (req, res) => {
-    
+router.post('/', async(req, res) => {
+    const subscriber = new Subscriber({
+        name: req.body.name,
+        subscribedToChannel: req.body.subscribedToChannel
+    })
+    try{
+        const newSubscriber = await subscriber.save()
+        res.status(201).json(newSubscriber)
+    }catch(err){
+        res.status(400).json({message: err.message}) //400 error, something wrong with user input
+    }
 })
 //updating one... patch updates specific parts while put updates all
 router.patch('/:id', (req, res) => {
